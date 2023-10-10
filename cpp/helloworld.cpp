4f1ab4ac316b48d1cb70a7ea7f08a4930d171830
Iansten Xing <546574881@qq.com>
2023-10-10 17:22:19 +0800

    first commit

---
+++ cpp/helloworld.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+    void Ouput() {
+        Display();  // 1、非静态成员函数调用静态成员函数
+    }
+    
+    static void Show() {
+        Display();  // 2、静态成员函数调用静态成员函数
+    }
+
+public:
+    static int m_x;
+
+    static void Display() {
+        cout << "m_x = " << m_x << endl;
+    }
+};
+
+int Base::m_x = 20;
+int main()
+{
+    Base obj;
+
+    obj.Show();
+
+	return 0;
+}
+
